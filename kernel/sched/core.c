7d2f7fb8dadc1503c78f5125954790740ecf212f
Bing Huang <huangbing@kylinos.cn>
2022-07-23 05:36:09 +0800

    sched/fair: Make per-cpu cpumasks static
    The load_balance_mask and select_rq_mask percpu variables are only used in
    kernel/sched/fair.c.
    
    Make them static and move their allocation into init_sched_fair_class().
    
    Replace kzalloc_node() with zalloc_cpumask_var_node() to get rid of the
    CONFIG_CPUMASK_OFFSTACK #ifdef and to align with per-cpu cpumask
    allocation for RT (local_cpu_mask in init_sched_rt_class()) and DL
    class (local_cpu_mask_dl in init_sched_dl_class()).
    
    [ mingo: Tidied up changelog & touched up the code. ]
    
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20220722213609.3901-1-huangbing775@126.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7285,9 +7285,6 @@ LIST_HEAD(task_groups);
 static struct kmem_cache *task_group_cache __read_mostly;
 #endif
 
-DECLARE_PER_CPU(cpumask_var_t, load_balance_mask);
-DECLARE_PER_CPU(cpumask_var_t, select_idle_mask);
-
 void __init sched_init(void)
 {
 	unsigned long alloc_size = 0, ptr;
@@ -7323,14 +7320,6 @@ void __init sched_init(void)
 
 #endif /* CONFIG_RT_GROUP_SCHED */
 	}
-#ifdef CONFIG_CPUMASK_OFFSTACK
-	for_each_possible_cpu(i) {
-		per_cpu(load_balance_mask, i) = (cpumask_var_t)kzalloc_node(
-			cpumask_size(), GFP_KERNEL, cpu_to_node(i));
-		per_cpu(select_idle_mask, i) = (cpumask_var_t)kzalloc_node(
-			cpumask_size(), GFP_KERNEL, cpu_to_node(i));
-	}
-#endif /* CONFIG_CPUMASK_OFFSTACK */
 
 	init_rt_bandwidth(&def_rt_bandwidth, global_rt_period(), global_rt_runtime());
 
