fd205a5c8aef9bed4561a09340757b46ef08ee58
Qais Yousef <qyousef@layalina.io>
2024-04-07 14:22:31 +0100

    sched/pelt: Add a new function to approximate the future util_avg value
    Given a util_avg value, the new function will return the future one
    given a runtime delta.
    
    This will be useful in later patches to help replace some magic margins
    with more deterministic behavior.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -328,6 +328,8 @@ struct dl_bw {
 	u64			total_bw;
 };
 
+unsigned long approximate_util_avg(unsigned long util, u64 delta);
+
 extern void dl_change_utilization(struct task_struct *p, u64 new_bw);
 extern void init_dl_bw(struct dl_bw *dl_b);
 extern int  sched_dl_global_validate(void);
