60648bba262ea6d3c1ecc04ab53750243d01bd47
Qais Yousef <qyousef@layalina.io>
2024-04-07 14:22:46 +0100

    sched/pelt: Add a new function to approximate runtime to reach given util
    It is basically the ramp-up time from 0 to a given value. Will be used
    later to implement new tunable to control response time  for schedutil.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -329,6 +329,7 @@ struct dl_bw {
 };
 
 unsigned long approximate_util_avg(unsigned long util, u64 delta);
+u64 approximate_runtime(unsigned long util);
 
 extern void dl_change_utilization(struct task_struct *p, u64 new_bw);
 extern void init_dl_bw(struct dl_bw *dl_b);
