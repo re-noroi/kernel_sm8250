ba07d7644040206328775501f9ed0bf5989bc5c9
re-noroi <mochtur@gmail.com>
2025-02-06 11:33:02 +0700

    implementing latest next-susfs branch
    Signed-off-by: re-noroi <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,3 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
-cp -rf "Implement-SUSFS-v1.5.4-for-KernelSU-Next.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "Implement-SUSFS-v1.5.4-for-KernelSU-Next.patch" && cd ..
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
