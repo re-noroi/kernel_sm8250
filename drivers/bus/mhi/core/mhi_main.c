76405c0dd6f68596614c409da33612f87da75eca
re-noroi <mochtur@gmail.com>
2025-03-02 16:29:15 +0700

    drivers: fix more warning
    they're coming late
    
    Signed-off-by: re-noroi <mochtur@gmail.com>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -2217,13 +2217,6 @@ int mhi_debugfs_mhi_regdump_show(struct seq_file *m, void *d)
 	return 0;
 }
 
-int mhi_debugfs_mhi_states_show(struct seq_file *m, void *d)
-{
-	struct mhi_controller *mhi_cntrl = m->private;
-
-	return 0;
-}
-
 int mhi_debugfs_mhi_event_show(struct seq_file *m, void *d)
 {
 	struct mhi_controller *mhi_cntrl = m->private;
