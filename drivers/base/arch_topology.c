c313459678548334a7500a268d60bf0cee0c921e
re-noroi <mochtur@gmail.com>
2025-03-19 12:05:14 +0700

    Revert "arch_topology: Introduce minimum frequency scale"
    This reverts commit d92459150425939dad16f4dab6e527c144a970df.

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -63,23 +63,6 @@ void topology_set_cpu_scale(unsigned int cpu, unsigned long capacity)
 	per_cpu(cpu_scale, cpu) = capacity;
 }
 
-DEFINE_PER_CPU(unsigned long, arch_min_freq_scale);
-EXPORT_PER_CPU_SYMBOL_GPL(arch_min_freq_scale);
-void topology_set_min_freq_scale(const struct cpumask *cpus,
-				 unsigned long min_freq, unsigned long max_freq)
-{
-	unsigned long scale;
-	int i;
-
-	if (WARN_ON_ONCE(!max_freq))
-		return;
-	
-	scale = (min_freq * per_cpu(cpu_scale, cpumask_any(cpus))) / max_freq;
-	
-	for_each_cpu(i, cpus)
-		per_cpu(arch_min_freq_scale, i) = scale;
-}
-
 DEFINE_PER_CPU(unsigned long, thermal_pressure);
 
 void arch_set_thermal_pressure(struct cpumask *cpus,
