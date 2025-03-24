2b568464874b336e1ba7f56773854e197348a7b2
re-noroi <mochtur@gmail.com>
2025-03-24 10:59:47 +0700

    sched/fair: Reduce unnecessary task migrations in select_task_rq_fair()
    - Optimize select_task_rq_fair() to minimize task migrations.
    - Prioritize prev_cpu if it's lightly loaded to avoid excessive overhead.
    - Reduce calls to find_idlest_cpu() when not necessary.
    
    Signed-off-by: re-noroi <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8584,10 +8584,13 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		record_wakee(p);
 
                 if (sched_energy_enabled()) {
-			new_cpu = find_energy_efficient_cpu(p, prev_cpu, sync, 1);
-			if (new_cpu >= 0)
-				return new_cpu;
-			new_cpu = prev_cpu;
+			if (cpu_rq(prev_cpu)->nr_running == 0)
+        		return prev_cpu;
+
+    		new_cpu = find_energy_efficient_cpu(p, prev_cpu, sync, 1);
+    		if (new_cpu >= 0)
+        		return new_cpu;
+    		new_cpu = prev_cpu;
 		}
 
 		want_affine = !wake_wide(p) &&
@@ -8617,11 +8620,14 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 	if (unlikely(sd)) {
 		/* Slow path */
-		new_cpu = find_idlest_cpu(sd, p, cpu, prev_cpu, sd_flag);
+		if (cpu_rq(prev_cpu)->nr_running <= 2)
+        	new_cpu = prev_cpu;
+    	else
+        	new_cpu = find_idlest_cpu(sd, p, cpu, prev_cpu, sd_flag);
 	} else if (sd_flag & SD_BALANCE_WAKE) { /* XXX always ? */
 		/* Fast path */
 
-		new_cpu = select_idle_sibling(p, prev_cpu, new_cpu);
+		new_cpu = (cpu_rq(prev_cpu)->nr_running < 3) ? prev_cpu : select_idle_sibling(p, prev_cpu, new_cpu);
 	}
 	rcu_read_unlock();
 
