375fc3e1957753c7223acce2ffd1531849ebac00
Ignacio Encinas <ignacio@iencinas.com>
2025-03-31 21:57:05 +0200

    mm: annotate data race in update_hiwater_rss
    mm_struct.hiwater_rss can be accessed concurrently without proper
    synchronization as reported by KCSAN.
    
    This data race is benign as it only affects accounting information.
    Annotate it with data_race() to make KCSAN happy.
    
    Link: https://lkml.kernel.org/r/20250331-mm-maxrss-data-race-v2-1-cf958e6205bf@iencinas.com
    Signed-off-by: Ignacio Encinas <ignacio@iencinas.com>
    Reported-by: syzbot+419c4b42acc36c420ad3@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/all/67e3390c.050a0220.1ec46.0001.GAE@google.com/
    Suggested-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Acked-by: Pedro Falcato <pfalcato@suse.de>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -14,6 +14,7 @@
 #include <linux/rbtree.h>
 #include <linux/atomic.h>
 #include <linux/debug_locks.h>
+#include <linux/compiler.h>
 #include <linux/mm_types.h>
 #include <linux/range.h>
 #include <linux/pfn.h>
@@ -1767,7 +1768,7 @@ static inline void update_hiwater_rss(struct mm_struct *mm)
 {
 	unsigned long _rss = get_mm_rss(mm);
 
-	if ((mm)->hiwater_rss < _rss)
+	if (data_race(mm->hiwater_rss) < _rss)
 		(mm)->hiwater_rss = _rss;
 }
 
