eb7a06055456c173930e3b134c6f17f140dee19c
Muhammad Guntur <mochtur@gmail.com>
2025-05-06 21:25:06 +0700

    fs/proc: fix C99 warning
    Move the declaration before any function.
    
    Fixes warning:
    ../fs/proc/task_mmu.c:383:18: warning: mixing declarations and code is a C99 extension [-Wdeclaration-after-statement]
      383 |                 struct dentry *dentry = file->f_path.dentry;
          |                                ^
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -381,6 +381,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
+	struct dentry *dentry = NULL;
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
@@ -396,7 +397,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 bypass_orig_flow:
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		struct dentry *dentry = file->f_path.dentry;
+		dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
             	if (strstr(path, "lineage") ||
