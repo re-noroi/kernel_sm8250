0eab24ad97407e9512939d07d622290b7cb6ffbb
Muhammad Guntur <mochtur@gmail.com>
2025-05-16 07:40:47 +0700

    sched: Clean up STUNE-related code
    STUNE is no longer in use, clean up the code for clarity.
    Follow-up of: ce904ebd590c ("sched: Clean up WALT-related code")
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1493,12 +1493,7 @@ struct task_struct {
 	struct mutex			futex_exit_mutex;
 #endif
 
-	/* bca62a0ae565 ("sched/tune: Fix improper accounting of tasks") */
-#ifdef CONFIG_SCHED_TUNE
-	ANDROID_KABI_USE(7, int stune_idx);
-#else
 	ANDROID_KABI_RESERVE(7);
-#endif
 #ifdef CONFIG_KSU_SUSFS
 	ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
 #else
