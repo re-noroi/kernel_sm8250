e7730c72df813cb484e2ffb2736387393c0cbc81
Muhammad Guntur <mochtur@gmail.com>
2025-06-12 19:02:03 +0700

    Revert "sched: Fix detection of empty queues in child qdiscs"
    SenX reported that this commit causing some kind of
    connection lags on AOSP, revert it and their fixes.
    
    Revert "net_sched: hfsc: Address reentrant enqueue adding class to eltree twice"
    Revert "net_sched: qfq: Fix double list add in class with netem as child qdisc"
    Revert "net_sched: hfsc: Fix a UAF vulnerability in class with netem as child qdisc"
    Revert "net_sched: drr: Fix double list add in class with netem as child qdisc"
    
    Reported-by: SenX <senseiix@dev.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- net/sched/sch_drr.c
+++ net/sched/sch_drr.c
@@ -39,11 +39,6 @@ struct drr_sched {
 	struct Qdisc_class_hash		clhash;
 };
 
-static bool cl_is_active(struct drr_class *cl)
-{
-	return !list_empty(&cl->alist);
-}
-
 static struct drr_class *drr_find_class(struct Qdisc *sch, u32 classid)
 {
 	struct drr_sched *q = qdisc_priv(sch);
@@ -376,7 +371,7 @@ static int drr_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 		return err;
 	}
 
-	if (!cl_is_active(cl)) {
+	if (cl->qdisc->q.qlen == 1) {
 		list_add_tail(&cl->alist, &q->active);
 		cl->deficit = cl->quantum;
 	}
