ffc06bf89c95feccc079993ea5c417c5e1dc213b
Muhammad Guntur <mochtur@gmail.com>
2025-06-21 07:28:40 +0700

    KSUN: Switch to ksun archive for now
    I'll fork it myself next time.
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,2 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs-dev
+curl -LSs "https://raw.githubusercontent.com/lazymeao-archive/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
