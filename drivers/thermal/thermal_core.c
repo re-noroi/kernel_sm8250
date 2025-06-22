d232e1e2540cab0a3a9c19a779b84671f327ecf4
Muhammad Guntur <mochtur@gmail.com>
2025-06-22 09:33:19 +0700

    thermal: Reverts some unnecessary changes
    Revert "BACKPORT: thermal/core: Rearm the monitoring only one time"
    Revert "thermal: core: Workaround for xiaomi thermal mitigation"
    Revert "thermal/qcom/msm_lmh_dcvs: Switch to non deferrable work"
    Revert "thermal: qcom: tracing: add clock_set_rate tracing for lmh"
    Revert "Thermal: Added VIRT_COUNT_THRESHOLD to virtual sensor"
    Revert "ANDROID: GKI: drivers: of-thermal: Relate thermal zones using same sensor"
    Revert "thermal: clear throttling when temperature < clear point"
    Revert "thermal: bcl_peripheral: Return before thermal trip evaluates"
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -459,7 +459,11 @@ static void handle_thermal_trip(struct thermal_zone_device *tz, int trip)
 		handle_critical_trips(tz, trip, type);
 	else
 		handle_non_critical_trips(tz, trip);
-
+	/*
+	 * Alright, we handled this trip successfully.
+	 * So, start monitoring again.
+	 */
+	monitor_thermal_zone(tz);
 	trace_thermal_handle_trip(tz, trip);
 }
 
@@ -552,8 +556,6 @@ void thermal_zone_device_update(struct thermal_zone_device *tz,
 
 	for (count = 0; count < tz->trips; count++)
 		handle_thermal_trip(tz, count);
-
-	monitor_thermal_zone(tz);
 }
 EXPORT_SYMBOL_GPL(thermal_zone_device_update);
 
@@ -1725,8 +1727,6 @@ thermal_sconfig_store(struct device *dev,
 	else
 		atomic_set(&switch_mode, val);
 
-	if (ret)
-		return ret;
 	return len;
 }
 
