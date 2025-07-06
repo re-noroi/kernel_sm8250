94ae22fbd722c267e1a7c7ab40c1c519c2c78585
Muhammad Guntur <mochtur@gmail.com>
2025-07-06 10:08:41 +0700

    cpufreq/sched: Add tunables to prime core manual headroom boost
    Some ppl like it for unknown reason
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2164,6 +2164,7 @@ extern unsigned int sysctl_sched_base_slice;
 
 extern unsigned int sysctl_boost_lpmask;
 extern unsigned int sysctl_boost_bpmask;
+extern unsigned int sysctl_boost_prime;
 extern unsigned int sysctl_manual_boost;
 extern unsigned int sysctl_suppress_boost;
 
