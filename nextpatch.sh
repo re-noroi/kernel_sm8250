b1aba870af59d2eebc074584cc9bd6e98ef959e0
Muhammad Guntur <mochtur@gmail.com>
2025-07-19 14:51:54 +0700

    nextpatch: Switch to my fork of ksun
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,2 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/lazymeao-archive/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/next-susfs_v1.5.5-v1.5.7/kernel/setup.sh" | bash -s next-susfs_v1.5.5-v1.5.7
