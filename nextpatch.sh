38292b3c9b622c4b0924595315a9c49661acfe14
Muhammad Guntur <mochtur@gmail.com>
2025-07-19 14:18:28 +0700

    nextpatch: Add more ksu manager support
    KSU, MKSU, RKSU, Fork by backslashxx and SukiSU.
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,2 +1,5 @@
 rm -rf KernelSU-Next
 curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
+echo "Adding more manager support..."
+cp -rf "ksun_more_manager.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "ksun_more_manager.patch" && cd ..
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx and SukiSU)"
