bd8a82c919a9c47543aeba516c9b04073ee6fbc3
Muhammad Guntur <mochtur@gmail.com>
2025-07-20 09:44:35 +0700

    nextpatch: Use ksun 1.0.9
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,5 +1,5 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.9
 echo "Adding more manager support..."
 cp -rf "ksun_more_manager.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "ksun_more_manager.patch" && cd ..
 echo "Done (KSU, MKSU, RKSU, Fork by backslashxx and SukiSU)"
