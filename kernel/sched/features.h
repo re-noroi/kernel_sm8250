7a8983bbb83a7a50ed3f7ab9697c5a8e24362fad
Muhammad Guntur <mochtur@gmail.com>
2025-08-08 19:48:42 +0700

    sched/eevdf: Remove ENFORCE_ELIGIBILITY
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-#define SCHED_FEAT_ENFORCE_ELIGIBILITY 0
 /*
  * Using the avg_vruntime, do the right thing and preserve lag across
  * sleep+wake cycles. EEVDF placement strategy #1, #2 if disabled.
