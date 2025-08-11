cabc22fba6ce92f9f49a506357c1cbd093a60ce0
Muhammad Guntur <mochtur@gmail.com>
2025-08-11 09:51:04 +0700

    sched: Remove init_max_cpu_capacity
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -499,8 +499,6 @@ static int init_rootdomain(struct root_domain *rd)
 	if (cpupri_init(&rd->cpupri) != 0)
 		goto free_cpudl;
 
-	init_max_cpu_capacity(&rd->max_cpu_capacity);
-
 	return 0;
 
 free_cpudl:
