8975ecd3ff9fa61b60c47f88da6610b62d73f6e4
Muhammad Guntur <mochtur@gmail.com>
2025-08-25 07:24:55 +0700

    techpack/audio: Checkout to n0kernel
    Dropped some qcom changes for LOS.
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -1128,7 +1128,7 @@ static int32_t afe_callback(struct apr_client_data *data, void *priv)
 #ifdef CONFIG_US_PROXIMITY
 	} else if (data->opcode == MI_ULTRASOUND_OPCODE) {
 		if (NULL != data->payload) {
-			pr_debug("[MIUS] mi ultrasound afe afe cb");
+			printk(KERN_DEBUG "[MIUS] mi ultrasound afe afe cb");
 			mius_process_apr_payload(data->payload);
 		} else
 			pr_err("[EXPORT_SYMBOLLUS]: payload ptr is Invalid");
@@ -2372,8 +2372,11 @@ static void afe_send_custom_topology(void)
 	this_afe.set_custom_topology = 0;
 	cal_block = cal_utils_get_only_cal_block(this_afe.cal_data[cal_index]);
 	if (cal_block == NULL || cal_utils_is_cal_stale(cal_block)) {
+		pr_err("%s cal_block not found!!\n", __func__);
 		goto unlock;
 	}
+
+	pr_debug("%s: Sending cal_index cal %d\n", __func__, cal_index);
 
 	ret = remap_cal_data(cal_block, cal_index);
 	if (ret) {
@@ -3242,7 +3245,7 @@ static int afe_get_cal_topology_id(u16 port_id, u32 *topology_id,
 	struct audio_cal_info_afe_top   *afe_top_info = NULL;
 
 	if (this_afe.cal_data[cal_type_index] == NULL) {
-		pr_debug("%s: cal_type %d not initialized\n", __func__,
+		pr_err("%s: cal_type %d not initialized\n", __func__,
 			cal_type_index);
 		return -EINVAL;
 	}
@@ -3256,7 +3259,7 @@ static int afe_get_cal_topology_id(u16 port_id, u32 *topology_id,
 	cal_block = afe_find_cal_topo_id_by_port(
 		this_afe.cal_data[cal_type_index], port_id);
 	if (cal_block == NULL) {
-		pr_debug("%s: cal_type %d not initialized for this port %d\n",
+		pr_err("%s: cal_type %d not initialized for this port %d\n",
 			__func__, cal_type_index, port_id);
 		ret = -EINVAL;
 		goto unlock;
@@ -3692,6 +3695,7 @@ static int send_afe_cal_type(int cal_index, int port_id)
 				this_afe.cal_data[cal_index]);
 
 	if (cal_block == NULL || cal_utils_is_cal_stale(cal_block)) {
+		pr_err_ratelimited("%s cal_block not found!!\n", __func__);
 		ret = -EINVAL;
 		goto unlock;
 	}
@@ -9043,7 +9047,7 @@ static int afe_get_clk_src(u16 port_id, char *clk_src)
 		return -EINVAL;
 	}
 
-	if (!*clkinfo_per_port[idx].clk_src_name)
+	if (clkinfo_per_port[idx].clk_src_name == NULL)
 		return -EINVAL;
 	strlcpy(clk_src, clkinfo_per_port[idx].clk_src_name,
 				CLK_SRC_NAME_MAX);
@@ -9364,7 +9368,7 @@ int afe_set_lpass_clock_v2(u16 port_id, struct afe_clk_set *cfg)
 		return -EINVAL;
 	}
 
-	if (*clk_src_name) {
+	if (clk_src_name != NULL) {
 		if (cfg->clk_freq_in_hz % AFE_SAMPLING_RATE_8KHZ) {
 			if (clk_src_name[CLK_SRC_FRACT] != NULL)
 				ret = afe_set_source_clk(port_id,
