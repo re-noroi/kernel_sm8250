c3b665d04ea7742a61552195ee47cd07df89895c
Greg Kroah-Hartman <gregkh@google.com>
2025-09-02 12:08:44 +0000

    Revert "PCI/ACPI: Fix runtime PM ref imbalance on Hot-Plug Capable ports"
    This reverts commit 607b3426547701efcc4f3d39ce05137497601909 which is
    commit 6cff20ce3b92ffbf2fc5eb9e5a030b3672aa414a upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I9fa208412f0ceeb38ca3aadc81726f0057917514
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -378,11 +378,13 @@ EXPORT_SYMBOL_GPL(pci_get_hp_params);
 bool pciehp_is_native(struct pci_dev *bridge)
 {
 	const struct pci_host_bridge *host;
+	u32 slot_cap;
 
 	if (!IS_ENABLED(CONFIG_HOTPLUG_PCI_PCIE))
 		return false;
 
-	if (!bridge->is_pciehp)
+	pcie_capability_read_dword(bridge, PCI_EXP_SLTCAP, &slot_cap);
+	if (!(slot_cap & PCI_EXP_SLTCAP_HPC))
 		return false;
 
 	if (pcie_ports_native)
