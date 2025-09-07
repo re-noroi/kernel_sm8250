8206ad21c2750a1ad894ecbc5361d94efdfe9f75
Muhammad Guntur <mochtur@gmail.com>
2025-09-07 18:20:36 +0700

    nextpatch: Add ksun workaround for new bpf patch
    We have 5.10 selinux changes, so use it instead of
    non-gki version.
    
    https://github.com/re-noroi/KernelSU-Next/commit/2c8a2c7459081536d502569208661b5abc33957a
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -3,3 +3,4 @@ curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/se
 echo "Adding more manager support..."
 cp -rf "ksun_more_manager.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "ksun_more_manager.patch" && cd ..
 echo "Done (KSU, MKSU, RKSU, Fork by backslashxx and SukiSU)"
+cp -rf "0001-KSUN-BPF.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "0001-KSUN-BPF.patch" && cd ..
