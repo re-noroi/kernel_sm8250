e0c7c64b595d25e467965bbb71c6e8cdbbad7bc7
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-09-09 20:56:07 +0700

    selinux: use spoofed permissive selinux for oplus build
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -122,12 +122,26 @@ static void selinux_fs_info_free(struct super_block *sb)
 static ssize_t sel_read_enforce(struct file *filp, char __user *buf,
 				size_t count, loff_t *ppos)
 {
+#ifndef CONFIG_E404_OPLUS
 	struct selinux_fs_info *fsi = file_inode(filp)->i_sb->s_fs_info;
+#endif
 	char tmpbuf[TMPBUFLEN];
 	ssize_t length;
+#ifdef CONFIG_E404_OPLUS
+	bool hide = false;
 
+	if (current->cred->uid.val >= 10000)
+		hide = true;
+	else if (strstr(current->comm, ".gms") != NULL)
+		hide = true;
+
+	length = scnprintf(tmpbuf, TMPBUFLEN, "%d", hide ? 1 : 0);
+
+#else
 	length = scnprintf(tmpbuf, TMPBUFLEN, "%d",
 			   enforcing_enabled(fsi->state));
+#endif
+
 	return simple_read_from_buffer(buf, count, ppos, tmpbuf, length);
 }
 
@@ -157,7 +171,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+#ifdef CONFIG_E404_OPLUS
+	new_value = 0;
+#else
 	new_value = !!new_value;
+#endif
 
 	old_value = enforcing_enabled(state);
 	if (new_value != old_value) {
