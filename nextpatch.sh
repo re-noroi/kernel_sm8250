639b4de448a9891ed8558c0b842066eb7a461172
Muhammad Guntur <mochtur@gmail.com>
2025-09-19 07:50:12 +0700

    nextpatch: Switch to my fork for convenience
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,2 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/next-susfs_v1.5.5-v1.5.7/kernel/setup.sh" | bash -s next-susfs_v1.5.5-v1.5.7
+curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
