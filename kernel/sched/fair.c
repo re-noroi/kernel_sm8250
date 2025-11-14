c8a4a66f2f78e1c305960e335d92e0daad7a4dca
Muhammad Guntur <mochtur@gmail.com>
2025-11-15 06:13:24 +0700

    sched/fair: only update newidle stats when cost != 0
    Fixes: 8a2fbf6423ae ("sched/fair: Proportional newidle balance")
    Link:https://lore.kernel.org/all/39bf6191-82d5-467c-9c09-2deb420875ba@os.amperecomputing.com/
    Signed-off-by: Adam Li <adamli@os.amperecomputing.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11290,7 +11290,8 @@ update_newidle_cost(struct sched_domain *sd, u64 cost, unsigned int success)
 	unsigned long next_decay = sd->last_decay_max_lb_cost + HZ;
 	unsigned long now = jiffies;
 
-	update_newidle_stats(sd, success);
+	if (cost)
+		update_newidle_stats(sd, success);
 
 	if (cost > sd->max_newidle_lb_cost) {
 		/*
