814ad3ebbd7b80252013ed5ccd8aab8cd70285bb
Prabuelsi20 <indahkhafilah@gmail.com>
2026-04-13 15:45:42 +0700

    Use ReSukiSU instead of KernelSU

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,2 +1,4 @@
-rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
+rm -rf KernelSU
+rm -rf drivers/kernelsu
+curl -LSs https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh -o /tmp/resuki_setup.sh
+bash /tmp/resuki_setup.sh || { echo "ReSukiSU inject failed!"; exit 1; }
