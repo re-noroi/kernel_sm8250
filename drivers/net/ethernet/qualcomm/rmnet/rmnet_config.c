3465f071284c84d7d9c67bef90a26f98c55cb682
Weiming Shi <bestswngs@gmail.com>
2026-05-14 05:25:12 -0700

    net: qualcomm: rmnet: fix endpoint use-after-free in rmnet_dellink()
    commit d00c953a8f69921f484b629801766da68f27f658 upstream.
    
    rmnet_dellink() removes the endpoint from the hash table with
    hlist_del_init_rcu() and then immediately frees it with kfree(). However,
    RCU readers on the receive path (rmnet_rx_handler ->
    __rmnet_map_ingress_handler) may still hold a reference to the endpoint and
    dereference ep->egress_dev after the memory has been freed. The endpoint is
    a kmalloc-32 object, and the stale read at offset 8 corresponds to the
    egress_dev pointer.
    
      BUG: unable to handle page fault for address: ffffffffde942eef
      Oops: 0002 [#1] SMP NOPTI
      CPU: 1 UID: 0 PID: 137 Comm: poc_write Not tainted 7.0.0+ #4 PREEMPTLAZY
      RIP: 0010:rmnet_vnd_rx_fixup (rmnet_vnd.c:27)
      Call Trace:
       <TASK>
       __rmnet_map_ingress_handler (rmnet_handlers.c:48 rmnet_handlers.c:101)
       rmnet_rx_handler (rmnet_handlers.c:129 rmnet_handlers.c:235)
       __netif_receive_skb_core.constprop.0 (net/core/dev.c:6096)
       __netif_receive_skb_one_core (net/core/dev.c:6208)
       netif_receive_skb (net/core/dev.c:6467)
       tun_get_user (drivers/net/tun.c:1955)
       tun_chr_write_iter (drivers/net/tun.c:2003)
       vfs_write (fs/read_write.c:688)
       ksys_write (fs/read_write.c:740)
       </TASK>
    
    Add an rcu_head field to struct rmnet_endpoint and replace kfree() with
    kfree_rcu() so the endpoint memory remains valid through the RCU grace
    period. Also remove the rmnet_vnd_dellink() call and inline only the
    nr_rmnet_devs decrement, since rmnet_vnd_dellink() would set
    ep->egress_dev to NULL during the grace period, creating a data race
    with lockless readers.
    
    Fixes: ceed73a2cf4a ("drivers: net: ethernet: qualcomm: rmnet: Initial implementation")
    Reported-by: Xiang Mei <xmei5@asu.edu>
    Signed-off-by: Weiming Shi <bestswngs@gmail.com>
    Link: https://patch.msgid.link/20260514122511.3083479-2-bestswngs@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    [re-noroi: Adapt to msm-4.19 with qcom logics]
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_config.c
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_config.c
@@ -254,9 +254,8 @@ static void rmnet_dellink(struct net_device *dev, struct list_head *head)
 	if (ep) {
 		hlist_del_init_rcu(&ep->hlnode);
 		rmnet_unregister_bridge(dev, port);
-		rmnet_vnd_dellink(mux_id, port, ep);
-		synchronize_rcu();
-		kfree(ep);
+		port->nr_rmnet_devs--;
+		kfree_rcu(ep, rcu);
 	}
 
 	if (!port->nr_rmnet_devs)
@@ -277,7 +276,6 @@ static void rmnet_force_unassociate_device(struct net_device *dev)
 	struct rmnet_port *port;
 	unsigned long bkt_ep;
 	LIST_HEAD(list);
-	HLIST_HEAD(cleanup_list);
 
 	if (!rmnet_is_real_dev_registered(real_dev))
 		return;
@@ -291,17 +289,10 @@ static void rmnet_force_unassociate_device(struct net_device *dev)
 
 	hash_for_each_safe(port->muxed_ep, bkt_ep, tmp_ep, ep, hlnode) {
 		unregister_netdevice_queue(ep->egress_dev, &list);
-		rmnet_vnd_dellink(ep->mux_id, port, ep);
+		port->nr_rmnet_devs--;
 
 		hlist_del_init_rcu(&ep->hlnode);
-		hlist_add_head(&ep->hlnode, &cleanup_list);
-	}
-
-	synchronize_rcu();
-
-	hlist_for_each_entry_safe(ep, tmp_ep, &cleanup_list, hlnode) {
-		hlist_del(&ep->hlnode);
-		kfree(ep);
+		kfree_rcu(ep, rcu);
 	}
 
 	/* Unregistering devices in context before freeing port.
