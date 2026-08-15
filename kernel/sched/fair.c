c976531f6171afeaf8a56df08f648bc269406509
Muhammad Guntur <mochtur@gmail.com>
2026-08-15 12:24:38 +0700

    Revert "sched: speed up pelt util increase"
    This reverts commit 654c4d0ee6bf4a7bbc312e9e2d5761124b8615ee.
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4658,7 +4658,6 @@ static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 
 	if (flags & UPDATE_UTIL_EST)
 		util_est_update(se);
-	cfs_rq_util_change(cfs_rq, 0);
 }
 
 /*
