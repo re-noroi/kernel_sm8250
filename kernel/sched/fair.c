563cfa244270114a3ccc349029b33d394291c846
Muhammad Guntur <mochtur@gmail.com>
2026-09-04 09:11:16 +0700

    sched: Lower little cores hr scaling slightly
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -67,7 +67,7 @@ DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 /*
  * DVFS headroom cluster scaling
  */
-int sysctl_hr_scale_lp __read_mostly = 40;
+int sysctl_hr_scale_lp __read_mostly = 30;
 int sysctl_hr_scale_big __read_mostly = 20;
 int sysctl_hr_scale_prime __read_mostly = 3;
 unsigned int sysctl_hr_scaling __read_mostly = 1;
